bf7225c404a2003798c8c4c3012dadd47ef8de76
Ali kakoo <alikaku@yahoo.com>
2024-02-02 19:48:35 +0330

    create basic files

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from utils import PrepProcesor, columns 
+
+import numpy as np
+import pandas as pd
+import joblib
+
+model = joblib.load('score2.joblib')
+st.title('Will you survive if you were among Titanic passengers or not :ship:')
+
+hour =  st.number_input("“Input Hours Study In Day”",step=1.,format="%.2f")
+practice =  st.number_input("“Input Hours practice In Day”",step=1.,format="%.2f")
+teamwork =  st.number_input("“Input Hours TeamWork In Day”",step=1.,format="%.2f")
+score = st.number_input("Input Score Estimate", 0,100) 
+
+
+def predict(): 
+    row = np.array([hour,practice,teamwork]) 
+    X = pd.DataFrame([row], columns = columns)
+    prediction = model.predict(X)
+    st.write(prediction)
+trigger = st.button('Predict', on_click=predict)
